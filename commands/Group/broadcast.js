fb9e6e44842461ae9b0af01986cfc4dd6dead2e9
ahahahaj <102854411+ahahahaj@users.noreply.github.com>
2022-10-20 10:02:30 +0300

    Update broadcast.js

--- commands/Group/broadcast.js
+++ commands/Group/broadcast.js
@@ -1,7 +1,7 @@
 const { tlang,sleep } = require('../../lib')
 
 module.exports = {
-    name: 'broadcast',
+    name: 'broadcast / رساله جماعيه ',
     category: 'owner',
     desc: 'Sends SECKTOR userbot group link.',
     async exec(citel, Void,args,isCreator) {
