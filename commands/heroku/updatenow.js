290843d6dea1b911dec12f577dd0f4363448ba1d
ahahahaj <102854411+ahahahaj@users.noreply.github.com>
2022-10-21 01:12:50 +0300

    Update updatenow.js

--- commands/heroku/updatenow.js
+++ commands/heroku/updatenow.js
@@ -1,18 +1,18 @@
 const { prefix } = require('../../config')
 const DB = require('../../lib')
 module.exports = {
-   name: 'updatenow',
+   name: 'تحديث_الان',
    category: 'owner',
    desc: 'Updates bot with repo\'s refreshed commits.',
    async exec(citel, Void,args,isCreator) {
-    if(!isCreator) return citel.reply('This command is only for my owner')
+    if(!isCreator) return citel.reply('الامر فقط لمطوري')
     let commits = await DB.syncgit()
   if (commits.total === 0)  {
-   citel.reply(`Hey ${citel.pushName}. You have latest version installed.`)
+   citel.reply(`هلا ${citel.pushName}. عندك اخر تحديث.`)
     } else { 
        citel.reply('Build Started...')
       let update = await DB.updatedb()
-        citel.reply(update)
+        citel.reply(تحديث)
     }
    } 
 }
