759b0148239e24ccfe5c7d0cd9b3d2d2924ba528
ahahahaj <102854411+ahahahaj@users.noreply.github.com>
2022-10-21 01:32:45 +0300

    Update support.js

--- commands/Group/support.js
+++ commands/Group/support.js
@@ -5,7 +5,7 @@ module.exports = {
     category: 'weeb',
     desc: 'Sends SECKTOR userbot group link.',
     async exec(citel, Void,args) {
-        citel.reply(`*Group Name / اسم القروب: 𝗕𝗢𝗧 𝗜𝗡𝗖*\n*Group Link:* https://chat.whatsapp.com/IJnyOohnm2Y9F6MWgruFj5`);
+        citel.reply(`*اسم القروب: 𝗕𝗢𝗧 𝗜𝗡𝗖*\n*Group Link:* https://chat.whatsapp.com/IJnyOohnm2Y9F6MWgruFj5`);
         await Void.sendMessage(`${citel.sender}`, {
           image: fs.readFileSync("../../lib/assets/SocialLogo 1.png"),
           caption: `*Group Name / اسم القروب: 𝗕𝗢𝗧 𝗜𝗡𝗖*\n*Group Link:* https://chat.whatsapp.com/IJnyOohnm2Y9F6MWgruFj5`,
