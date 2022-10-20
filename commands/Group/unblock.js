6721f5a31ee2e5be2a439718e1135c0d7f05ca96
ahahahaj <102854411+ahahahaj@users.noreply.github.com>
2022-10-20 10:15:19 +0300

    Update unblock.js

--- commands/Group/unblock.js
+++ commands/Group/unblock.js
@@ -5,7 +5,7 @@ module.exports = {
     category: 'owner',
     desc: 'Unblocked to the quoted user..',
     async exec(citel, Void,args,isCreator) {
-        if (!citel.quoted) return citel.reply("Please reply to user");
+        if (!citel.quoted) return citel.reply("Please reply to user / رجاء منشن اليوزر");
         if (!isCreator) citel.reply(tlang().owner);
         let users = citel.mentionedJid[0] ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : args.join(" ").replace(/[^0-9]/g, "") + "@s.whatsapp.net";
         await Void.updateBlockStatus(users, "unblock")
