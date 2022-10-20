cf86140eddd1f414d9844174764c600492a800ad
ahahahaj <102854411+ahahahaj@users.noreply.github.com>
2022-10-20 13:36:37 +0300

    Update switch-deact.js

--- commands/Group/switch-deact.js
+++ commands/Group/switch-deact.js
@@ -2,7 +2,7 @@ const { sck,getAdmin,tlang,prefix} = require('../../lib')
 const Config = require('../../config')
 
 module.exports = {
-    name: 'deact',
+    name: 'قفل',
     category: 'group',
     desc: 'Switches for varios works.',
     async exec(citel, Void,args,isCreator) {
@@ -13,9 +13,9 @@ const botNumber =  await Void.decodeJid(Void.user.id)
 const isBotAdmins = citel.isGroup ? groupAdmins.includes(botNumber) : false;
 const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
 //-----------------------------------------  
-			if (!citel.isGroup) return citel.reply("This feature in only for Group.")
+			if (!citel.isGroup) return citel.reply("الميزه فقط للقروبات .")
 			if (!args.join(" ")) return citel.reply(`❌ Please provide me term like like\n1-events\n2-antilink\n3-nsfw\n4-cardgame\n5-bot`)
-			if (!isAdmins || !isCreator) return citel.reply("❌ This Command is only for Admin")
+			if (!isAdmins || !isCreator) return citel.reply("❌ الميزه فقط للادمن")
 			if (args[0] == "antilink") {
 				sck.findOne({ id: citel.chat })
 					.then(async (usr) => {
