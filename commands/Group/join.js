969fa5aa5a6b99cfb6d65e9a5aa1ec6b21b329ef
Raizel-2023 <111248812+Raizel-2023@users.noreply.github.com>
2022-10-26 13:16:41 +0300

    Update join.js

--- commands/Group/join.js
+++ commands/Group/join.js
@@ -3,15 +3,15 @@ const { tlang } = require('../../lib')
 module.exports = {
     name: 'ادخل',
     category: 'owner',
-    desc: 'joins given group link.',
+    desc: 'اعطني رابط القروب.',
     async exec(citel,Void,args,isCreator) {
       if (!isCreator) return citel.reply(tlang().owner);
 	 if (!args[0]) return citel.reply('Provide me url.')
-            if (!/https?:\/\/(chat\.whatsapp\.com)\/[A-Za-z]/.test(args[0])) return citel.reply(`Please give me valid url, ${tlang().greet}`);
+            if (!/https?:\/\/(chat\.whatsapp\.com)\/[A-Za-z]/.test(args[0])) return citel.reply(`يزق جيب رابط القروب, ${tlang().greet}`);
 				  let result = args[0].split("https://chat.whatsapp.com/")[1];
 				  await Void.groupAcceptInvite(result)
-					.then((res) => citel.reply("🟩Joined Group"))
-					.catch((err) => citel.reply("Error in Joining Group."));
+					.then((res) => citel.reply("🟩دخلت القروب"))
+					.catch((err) => citel.reply("لم استطع الانضمام للقروب."));
  
     }
  }
